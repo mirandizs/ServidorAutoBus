7a6b919f885b8d9555d37314b0f9e9e0e1374551
Sofia Miranda <149806274+mirandizs@users.noreply.github.com>
2025-05-10 17:37:18 +0100

    Endpoint inicial para compras

--- Servidor/Endpoints/Recibos.ts
+++ Servidor/Endpoints/Recibos.ts
@@ -24,4 +24,52 @@ router.get('/recibos', async (Pedido, Resposta) => {
     Resposta.send(Resultado)
 });
 
+
+
+router.post('/comprar', async (Pedido, Resposta) => {
+
+    const informacoesPedido = Pedido.body // body do pedido, com os dados passados~
+    const idUtilizador = Pedido.session.dados_utilizador?.id_utilizador
+
+    
+    //query para inserir, caso o utilizador queira, os dados do cartão à db 
+
+
+
+    // query para buscar os dados do carrinho
+    const query = `
+        SELECT * FROM carrinho WHERE id_utilizador = ?
+    `;
+
+    const [Carrinho] = await DB.execute<any[]>(query, [idUtilizador]) // executa a query com os valores passados. tambem pode usar "as any[]" para evitar erros do typescript
+    console.log(Carrinho)
+
+
+    // query para clonar os dados do carrinho para a entidade compras
+    var queryCompras = `
+        INSERT INTO compras (id_utilizador, id_ponto_partida, id_ponto_chegada, preco) 
+        VALUES 
+    `;
+
+    
+    const ValoresPassados:any[] = []
+
+    Carrinho.forEach((Bilhete: any) => {
+        queryCompras += `(?, ?, ?, ?),`;
+        ValoresPassados.push(
+            idUtilizador, 
+            Bilhete.id_ponto_partida, 
+            Bilhete.id_ponto_chegada, 
+            Bilhete.preco
+        )
+    });
+    queryCompras = queryCompras.slice(0, -1); // remove a última vírgula para nao dar erro de sintaxe de SQL
+
+    const [Resultado] = await DB.execute(queryCompras, ValoresPassados) // executa a query com os valores passados. tambem pode usar "as any[]" para evitar erros do typescript
+
+
+    //query para remover os dados do carrinho após ser adicionado à entidade compras
+
+});
+
 module.exports = router;
