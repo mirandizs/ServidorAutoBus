41604a2d91205562d65b8e315f79ccf0b09922b3
Sofia Miranda <149806274+mirandizs@users.noreply.github.com>
2025-05-14 14:22:23 +0100

    endpoint das compras finalizado

--- Servidor/Endpoints/Recibos.ts
+++ Servidor/Endpoints/Recibos.ts
@@ -2,27 +2,27 @@ import express from 'express'
 const router = express.Router();
 import { DB } from '../Globais.ts';
 
-router.get('/recibos', async (Pedido, Resposta) => {
-    const id = Pedido.session.dados_utilizador?.id_utilizador
+// router.get('/recibos', async (Pedido, Resposta) => {
+//     const id = Pedido.session.dados_utilizador?.id_utilizador
 
-    const query = `
-        SELECT 
-            r.id_recibo,
-            r.data,
-            r.valor,
-            r.estado,
-            p.local AS local_partida,
-            p2.local AS local_chegada
-        FROM recibos AS r
-        INNER JOIN pontos_rotas AS p ON r.id_ponto_partida = p.id_ponto
-        INNER JOIN pontos_rotas AS p2 ON r.id_ponto_chegada = p2.id_ponto
-        WHERE r.id_utilizador = ${id}
-    `
+//     const query = `
+//         SELECT 
+//             r.id_recibo,
+//             r.data,
+//             r.valor,
+//             r.estado,
+//             p.local AS local_partida,
+//             p2.local AS local_chegada
+//         FROM recibos AS r
+//         INNER JOIN pontos_rotas AS p ON r.id_ponto_partida = p.id_ponto
+//         INNER JOIN pontos_rotas AS p2 ON r.id_ponto_chegada = p2.id_ponto
+//         WHERE r.id_utilizador = ${id}
+//     `
 
-    const [Resultado] = await DB.query(query)
+//     const [Resultado] = await DB.query(query)
 
-    Resposta.send(Resultado)
-});
+//     Resposta.send(Resultado)
+// });
 
 
 
@@ -33,6 +33,29 @@ router.post('/comprar', async (Pedido, Resposta) => {
 
     
     //query para inserir, caso o utilizador queira, os dados do cartão à db 
+    const queryGuardarCartao = `
+        INSERT INTO pagamentos (metodo, nome_cartao, numero_cartao, validade, cvv, id_utilizador) 
+        VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    console.log(informacoesPedido.guardarCartao)
+
+    if (informacoesPedido.guardarCartao) {
+        const [GuardarCartao] = await DB.execute(queryGuardarCartao, [
+            informacoesPedido.metodo,
+            informacoesPedido.nome_cartao,
+            informacoesPedido.numero_cartao,
+            informacoesPedido.validade,
+            informacoesPedido.cvv,
+            idUtilizador
+        ]) // executa a query com os valores passados.
+        // console.log(GuardarCartao)
+    }
+    else {
+        console.log("Os dados do cartão não foram guardados.")
+        return;
+    }
+
 
 
 
@@ -45,6 +68,9 @@ router.post('/comprar', async (Pedido, Resposta) => {
     console.log(Carrinho)
 
 
+
+
+    
     // query para clonar os dados do carrinho para a entidade compras
     var queryCompras = `
         INSERT INTO compras (id_utilizador, id_ponto_partida, id_ponto_chegada, preco) 
@@ -65,11 +91,20 @@ router.post('/comprar', async (Pedido, Resposta) => {
     });
     queryCompras = queryCompras.slice(0, -1); // remove a última vírgula para nao dar erro de sintaxe de SQL
 
-    const [Resultado] = await DB.execute(queryCompras, ValoresPassados) // executa a query com os valores passados. tambem pode usar "as any[]" para evitar erros do typescript
+    const [Resultado] = await DB.execute(queryCompras, ValoresPassados) // executa a query com os valores passados. 
+    console.log(Resultado)
+
+
+
 
 
     //query para remover os dados do carrinho após ser adicionado à entidade compras
+    const queryRemoverCarrinho = `
+        DELETE FROM carrinho WHERE id_utilizador = ?
+    `;
 
+    await DB.execute(queryRemoverCarrinho, [idUtilizador]) // executa a query com os valores passados.
+    Resposta.send()
 });
 
 module.exports = router;
