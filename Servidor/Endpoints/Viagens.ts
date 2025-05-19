11a8714cca5cc84e09f4c32672a2f2181ca9fcb5
Sofia Miranda <149806274+mirandizs@users.noreply.github.com>
2025-05-19 18:15:55 +0100

    viagens e compras já funciona. endpoint terminado (?)

--- Servidor/Endpoints/Viagens.ts
+++ Servidor/Endpoints/Viagens.ts
@@ -12,20 +12,21 @@ router.get('/viagens', async (Pedido, Resposta) => {
     const hora_ida = Pedido.query.hora_ida
 
     const QUERY = `
-    SELECT 
-        p1.idautocarro,
-        p1.local AS local_partida,
-        p2.local AS local_chegada,
-        p1.hora_partida AS hora_partida
+      SELECT 
+          p1.idautocarro,
+          p1.local AS local_partida,
+          p2.local AS local_chegada,
+          p1.hora_partida AS hora_partida
 
-    FROM pontos_rotas p1
-    INNER JOIN pontos_rotas p2 ON p1.idautocarro = p2.idautocarro
-    INNER JOIN autocarro a ON a.idautocarro = p1.idautocarro
-    WHERE p1.local = '${local_partida}' 
-      AND p2.local = '${local_chegada}'
-      AND p1.hora_partida >= '${hora_ida}'
-      AND p1.hora_partida < p2.hora_partida
-    ORDER BY p1.hora_partida ASC;`
+      FROM pontos_rotas p1
+      INNER JOIN pontos_rotas p2 ON p1.idautocarro = p2.idautocarro
+      INNER JOIN autocarro a ON a.idautocarro = p1.idautocarro
+      WHERE p1.local = '${local_partida}' 
+        AND p2.local = '${local_chegada}'
+        AND p1.hora_partida >= '${hora_ida}'
+        AND p1.hora_partida < p2.hora_partida
+      ORDER BY p1.hora_partida ASC;
+    `
 
     const [Resultado] = await DB.query(QUERY)
 
